33296a327b47c30eb7d3fc5c7c78807a62622d33
lasse1511 <lassebonner@hotmail.com>
2021-05-25 13:06:11 +0200

    CLA parsing

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import argparse as ap
 import pandas as pd
 from clickhouse_driver import Client
 
+
 if __name__ == "__main__":
-    print("HELLOOOO")
+
+    # Passing command line arguments
+    parser = ap.ArgumentParser(
+        description="This app lists countries in specified order")
+    parser.add_argument("--sort-order", metavar="S",
+                        type=str, help="The sort order", default="asc")
+    args = parser.parse_args()
+
+    if (not args.sort_order in ["asc", "desc"]):
+        raise Exception("Please specify a valid sort order")
+
+    
+    print("OLLLLLEH", args.sort_order)
     input("WAIT")
