bffef2ccc229bb51f8c7c73a36907e68da8877b1
lasse1511 <lassebonner@hotmail.com>
2021-05-27 13:33:52 +0200

    Refactored argparser to click

--- main.py
+++ main.py
@@ -4,53 +4,42 @@ import databaseManager as db
 import prettytable
 from io import StringIO
 import pycountry
+import click 
 
-def convert_ISO3_to_ISO2(iso3_code):
-    print(iso3_code)
-    try:
-        return pycountry.countries.get(alpha_3=iso3_code).alpha_2
-    except:
-        return "--"
-
-
-if __name__ == "__main__":
-    # Passing command line arguments
-    parser = ap.ArgumentParser(
-        description="This app lists countries in specified order")
-    parser.add_argument("--sort-order", metavar="S",
-                        type=str, help="The sort order", default="asc")
-    args = parser.parse_args()
-
-    if (not args.sort_order in ["asc", "desc"]):
-        raise Exception("Please specify a valid sort order")
-    sort_order = args.sort_order
+# Passing command line arguments
+@click.command()
+@click.option("--sort-order", default='asc', help="The sort order of the list of countries.")
+def main(sort_order):
+    # Checks the value of sort_order in order to avoid malicious attempts
+    sort_order = sort_order.lower()
+    if (not sort_order in ['asc', 'desc']):
+        raise ValueError("Please enter a valid value for sort_order --- Valid option are (CI): \"asc\" or \"desc\"")
 
     # Connection, migration and seeding to db
     dbManager = db.databaseManager()
 
     # Select data from table countries and load into pandas dataframe
-    countries_df = pd.DataFrame.from_records(dbManager.client.execute("""
-        SELECT 
-            name,
-            alpha3Code
-        FROM countries
-        ORDER BY name """ + sort_order))
+    countries_df = pd.DataFrame.from_records(dbManager.client.execute("SELECT name, alpha3Code FROM countries ORDER BY name {}".format(sort_order)))
 
     #Convert column ISO3 to ISO2 format
     countries_df[1] = countries_df[1].apply(convert_ISO3_to_ISO2)
-    # print(countries_df[1])
-    # input("")
-
 
     # Renaming column of df to fit the description
     countries_df = countries_df.rename(columns = {0: 'Country name', 1: 'ISO2'}, inplace = False)
 
-    # print(countries_df)
     # Print the results using PrettyTable
     output = StringIO()
     countries_df.to_csv(output)
     output.seek(0)
     pt = prettytable.from_csv(output)
     print(pt)
-    # print("OLLLLLEH", args.sort_order)
-    # input("WAIT")
+
+def convert_ISO3_to_ISO2(iso3_code):
+    try:
+        return pycountry.countries.get(alpha_3=iso3_code).alpha_2
+    except:
+        return "--"
+
+
+if __name__ == "__main__": 
+    main()
