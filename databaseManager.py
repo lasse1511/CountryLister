6eb23553aa098bde8411c83fe426a7ec73c26b34
lasse1511 <lassebonner@hotmail.com>
2021-05-27 14:33:09 +0200

    Seperate check for seeding

--- databaseManager.py
+++ databaseManager.py
@@ -6,11 +6,11 @@ class databaseManager:
     def __init__(self):
         self.client = Client('localhost')
         self.migrateSchema()
+        self.executeSeeding()
 
     # migrates table definitions to ClickHouse db
     def migrateSchema(self):
         current_tables = self.client.execute("SHOW TABLES")
-        print(current_tables)
         if(len(current_tables) == 0):
             self.client.execute("""
                 CREATE TABLE countries (
@@ -21,18 +21,18 @@ class databaseManager:
                 ) 
             ENGINE = MergeTree;
             """)
-            self.executeSeeding()
 
     # inserts seed data in ClickHouse db
     def executeSeeding(self):
-        # country api endpoint
-        URL = "https://restcountries.eu/rest/v2/all"
+        rows_countries = self.client.execute("SELECT COUNT(*) FROM countries")
+        if(rows_countries[0][0] == 0):
+            # country api endpoint
+            URL = "https://restcountries.eu/rest/v2/all"
 
-        # getting json object (name and alpha3Code) with list of all countries
-        countries_df = pd.DataFrame.from_records(requests.get(url=URL).json()).filter(["name", "alpha3Code"], axis=1)
+            # getting json object (name and alpha3Code) with list of all countries
+            countries_df = pd.DataFrame.from_records(requests.get(url=URL).json()).filter(["name", "alpha3Code"], axis=1)
 
-        # Adds id column equal to element index
-        countries_df["id"] = countries_df.index + 1
-
-        # Inserts values into database
-        self.client.execute("INSERT INTO countries (id, name, alpha3Code) VALUES", countries_df.to_dict('records'))
+            # Adds id column equal to element index
+            countries_df["id"] = countries_df.index + 1
+            # Inserts values into database
+            self.client.execute("INSERT INTO countries (name, alpha3Code, id) VALUES", countries_df.to_dict('records'))
