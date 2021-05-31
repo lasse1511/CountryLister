892f84843fe3905b5db6e70d3cad5ed6b598918b
lasse1511 <lassebonner@hotmail.com>
2021-05-31 13:51:40 +0200

    UUID as Id for country

--- databaseManager.py
+++ databaseManager.py
@@ -1,6 +1,7 @@
 from clickhouse_driver import Client
 import requests
 import pandas as pd
+import uuid
 
 class databaseManager:
     def __init__(self):
@@ -14,12 +15,14 @@ class databaseManager:
         if(len(current_tables) == 0):
             self.client.execute("""
                 CREATE TABLE countries (
-                    id Int16 NOT NULL,
+                    id String NOT NULL,
                     name String NOT NULL,
                     alpha3Code String NOT NULL,
+                    sign Int8 DEFAULT -1,
+
                     PRIMARY KEY(id)
                 ) 
-            ENGINE = MergeTree;
+            ENGINE = CollapsingMergeTree(sign);
             """)
 
     # inserts seed data in ClickHouse db
@@ -30,9 +33,17 @@ class databaseManager:
             URL = "https://restcountries.eu/rest/v2/all"
 
             # getting json object (name and alpha3Code) with list of all countries
-            countries_df = pd.DataFrame.from_records(requests.get(url=URL).json()).filter(["name", "alpha3Code"], axis=1)
+            countries_df = pd.DataFrame.from_records(requests.get(
+                url=URL).json()).filter(["name", "alpha3Code"], axis=1)
 
-            # Adds id column equal to element index
-            countries_df["id"] = countries_df.index + 1
+            # Adds UUID as id
+            countries_df["id"] = countries_df.apply(self.generateUUID, axis=1) 
+
             # Inserts values into database
-            self.client.execute("INSERT INTO countries (name, alpha3Code, id) VALUES", countries_df.to_dict('records'))
+            self.client.execute(
+                "INSERT INTO countries (name, alpha3Code, id) VALUES", countries_df.to_dict('records'))
+
+
+    # Generates UUID from country name and alpha3Code to ensure each country has unique identifier
+    def generateUUID(self, inputRow):
+        return str(uuid.uuid3(uuid.NAMESPACE_DNS, inputRow["name"] + "##" + inputRow["alpha3Code"]))
